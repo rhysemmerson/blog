a543c29a3700a238949851a314fcac1779af4bc1
Rhys Emmerson <rhysemmerson@Rhyss-MacBook-Pro.local>
2022-08-03 09:39:38 +1000

    change meta tags

--- web/app/Layout.tsx
+++ web/app/Layout.tsx
@@ -3,6 +3,7 @@ import { PropsWithChildren, ReactNode } from "react";
 export default function ({ children }: PropsWithChildren<ReactNode>) {
     return <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
         <h1>Rhys Emmerson</h1>
+        
         {children}
 
         <p >Github: <a target="_blank" href="https://github.com/rhysemmerson/blog">@rhysemmerson</a></p>
